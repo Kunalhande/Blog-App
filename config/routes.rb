89a420dd33f5aa2fcdc98ff46080b09e9560499a
Kunalhande <kunalhand30@gmail.com>
2026-06-12 00:38:13 +0530

    signup feature for the application

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/navigation', to: 'navigation#navi'
-
   get '/about', to: 'pages#about'
 
   resources :articles
+
+  get '/signup', to: 'users#new'
+  resources :users
 end
