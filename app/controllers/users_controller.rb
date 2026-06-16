d2d5d69842251770380cef26c694221f9117515c
Kunalhande <kunalhand30@gmail.com>
2026-06-17 04:56:26 +0530

    users page is added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
@@ -21,21 +25,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
-
-    unless @user
-      flash[:alert] = "User not found"
-      redirect_to articles_path
-    end
+    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find_by(id: params[:id])
-
-    unless @user
-      flash[:alert] = "User not found"
-      redirect_to articles_path and return
-    end
+    @user = User.find(params[:id])
 
     if @user.update(user_params)
       flash[:notice] = "User updated successfully"
